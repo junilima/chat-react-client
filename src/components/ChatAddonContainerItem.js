f6222b49d8a222b1ee123fca644c8714f5b041f6
Juni Lima <junior.lima@outlook.com>
2021-07-14 03:58:53 -0300

    Added ChatEmoticonsItem and implemented Emoticons, Stickers and QuickMessages into ChatMessage

--- src/components/ChatAddonContainerItem.js
+++ src/components/ChatAddonContainerItem.js
@@ -1,17 +1,8 @@
 import React from 'react'
 import styles from '../styles.css'
 
-const ChatAddonContainerItem = ({ grid, ...props }) => {
-  return (
-    <div
-      className={
-        styles[
-          grid ? 'chat-stickers-container' : 'chat-quick-messages-container'
-        ]
-      }
-      {...props}
-    />
-  )
+const ChatAddonContainerItem = (props) => {
+  return <div className={styles['chat-addon-container-item']} {...props} />
 }
 
 export default ChatAddonContainerItem
